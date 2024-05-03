fbf3c04ecc48df11d214ad3e63651858bddad89e
Josiah Elias <josiahelias11@gmail.com>
2024-05-03 23:21:28 +0100

    installed and set up packages and environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-green-600">
+        <Typography>Hello world</Typography>
+      </div>
+    </>
+  );
+}
+
+export default App;
