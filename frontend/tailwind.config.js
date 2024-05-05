58b20079eada05f20c5250e9e497c57077a8fd8e
Josiah Elias <josiahelias11@gmail.com>
2024-05-05 11:40:54 +0100

    reset configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      textColor: {
-        white: "#30D42B",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
